9cbf4ea9b3057c81c17fbcb8771c3b693c67b5ed
Steve <stephenjcostigan@hotmail.com>
2018-05-16 20:56:55 -0500

    5-15-2018 Test Commit

---
+++ pioneerTrail/src/cit260/pioneertrail/model/Map.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.pioneertrail.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author Stephen
+ */
+public class Map implements Serializable{
+    
+}
