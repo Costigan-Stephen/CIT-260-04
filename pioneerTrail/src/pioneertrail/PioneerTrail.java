9c57b423616c64097883a0bee27f0083c3325259
Steve <stephenjcostigan@hotmail.com>
2018-05-18 11:50:48 -0500

    5-15-2018 Test Commit

--- pioneerTrail/src/pioneertrail/PioneerTrail.java
+++ pioneerTrail/src/pioneertrail/PioneerTrail.java
@@ -6,7 +6,13 @@
 package pioneertrail;
 
 import cit260.pioneertrail.model.Actor;
+import cit260.pioneertrail.model.Answers;
+import cit260.pioneertrail.model.Game;
+import cit260.pioneertrail.model.InventoryItem;
+import cit260.pioneertrail.model.Location;
+import cit260.pioneertrail.model.Map;
 import cit260.pioneertrail.model.Player;
+import cit260.pioneertrail.model.Scene;
 
 /**
  *
@@ -38,10 +44,26 @@ public class PioneerTrail {
                             ", Status : " + playerOneStatus);
         */
         
+        /* START STEVE */
+        InventoryItem hammer = new InventoryItem();
+        hammer.setInventoryType("tool");
+        hammer.setQuantityOfItems(1);
+        hammer.setItemWeight(10.0);
+        hammer.setDamageValue(100.0);
+        hammer.setWeightRemaining(100);/* Need to attach to a class to calculate in future compared to rest of inventory*/
+        hammer.setItemName("Hammer");
+        
+        Answers randomEncounter = new Answers();
+        randomEncounter.setQuestion("One of your Wagon wheels buckles, you...");
+        randomEncounter.setAnswer("Use hammer to straighten it"); /* This could potentially be used multiple times with varying results*/
+        randomEncounter.setResultOfAnswer("The wheel snaps from the force of the hammer.  It must be replaced.");
+        /* END STEVE */
+        
+        
         System.out.println(playerOne.toString());
-        
         System.out.println(Actor.Dog.toString());
-        
+        System.out.println(randomEncounter.toString());   /* STEVE */
+        System.out.println(hammer.toString());            /* STEVE */
         
     }
     
