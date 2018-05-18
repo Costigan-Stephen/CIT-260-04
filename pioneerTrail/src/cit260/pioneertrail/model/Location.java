0b89c163579627b17d5517bd98aac2b450020092
Marilee <marileeaustin@gmail.com>
2018-05-18 15:54:44 -0600

    5-15-18 Created Project

--- pioneerTrail/src/cit260/pioneertrail/model/Location.java
+++ pioneerTrail/src/cit260/pioneertrail/model/Location.java
@@ -1,18 +1,94 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cit260.pioneertrail.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Objects;
 
-/**
- *
- * @author Stephen
- */
 public class Location implements Serializable{
+    
+    private String name;
+    private int row;
+    private int column;
+    private int amountRemaining;
+
+    public Location() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getAmountRemaining() {
+        return amountRemaining;
+    }
+
+    public void setAmountRemaining(int amountRemaining) {
+        this.amountRemaining = amountRemaining;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.name);
+        hash = 41 * hash + this.row;
+        hash = 41 * hash + this.column;
+        hash = 41 * hash + this.amountRemaining;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.column != other.column) {
+            return false;
+        }
+        if (this.amountRemaining != other.amountRemaining) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "name=" + name + ", row=" + row + ", column=" + column + ", amountRemaining=" + amountRemaining + '}';
+    }
+
+
+
     
 }
