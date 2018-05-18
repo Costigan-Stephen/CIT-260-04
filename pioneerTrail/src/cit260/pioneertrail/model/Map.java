a776c71a1930b3e49a02c101c150490c51924723
Marilee <marileeaustin@gmail.com>
2018-05-18 15:55:25 -0600

    5-18-18 Map Class Created

--- pioneerTrail/src/cit260/pioneertrail/model/Map.java
+++ pioneerTrail/src/cit260/pioneertrail/model/Map.java
@@ -6,7 +6,7 @@
 package cit260.pioneertrail.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -15,4 +15,114 @@ import java.util.Objects;
  */
 public class Map implements Serializable{
     
+    // class instance variables
+    private String description;
+    private int rowCount;
+    private int columnCount;
+    private int currentRow;
+    private int currentColumn;
+    private Game game;
+
+    public Map() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    public int getCurrentRow() {
+        return currentRow;
+    }
+
+    public void setCurrentRow(int currentRow) {
+        this.currentRow = currentRow;
+    }
+
+    public int getCurrentColumn() {
+        return currentColumn;
+    }
+
+    public void setCurrentColumn(int currentColumn) {
+        this.currentColumn = currentColumn;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.description);
+        hash = 59 * hash + this.rowCount;
+        hash = 59 * hash + this.columnCount;
+        hash = 59 * hash + this.currentRow;
+        hash = 59 * hash + this.currentColumn;
+        hash = 59 * hash + Objects.hashCode(this.game);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.rowCount != other.rowCount) {
+            return false;
+        }
+        if (this.columnCount != other.columnCount) {
+            return false;
+        }
+        if (this.currentRow != other.currentRow) {
+            return false;
+        }
+        if (this.currentColumn != other.currentColumn) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.game, other.game)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "description=" + description + ", rowCount=" + rowCount + ", columnCount=" + columnCount + ", currentRow=" + currentRow + ", currentColumn=" + currentColumn + ", game=" + game + '}';
+    }
+
+ 
 }
