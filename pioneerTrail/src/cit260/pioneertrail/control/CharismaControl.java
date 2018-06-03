945a676a8efb142f7f3e64a35a990b49eb564908
hughe <hughe@LAPTOP-D25Q3228>
2018-06-02 17:45:48 -0700

    5/21/18-added scene class

---
+++ pioneerTrail/src/cit260/pioneertrail/control/CharismaControl.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.pioneertrail.control;
+
+/**
+ *
+ * @author hughes
+ */
+public class CharismaControl {
+    boolean calculateCharisma(boolean gender, char occupation, double money){
+        
+//If (gender = female) { charisma+ 10 }
+//If(gender = male) {charisma + 5}
+//If (occupation = lawyer) { charisma + 15 }
+//If (occupation = blacksmith {charisma + 10
+//If (occupation =  farmer) { charisma + 1 }
+//If (money * .10 + charisma)
+//    return charisma > 20
+
+
+//if gender true == female
+    double charisma = 0;
+        if(gender == true) {
+            charisma = charisma + 10.0;
+        }
+        //male
+      if(gender == false) {
+           charisma = charisma + 5.0;
+        }
+      //lawyer
+        if (occupation == 'l')
+                charisma = charisma + 15.0;
+        //blacksmith
+         if (occupation == 'b')
+                charisma = charisma + 10.0;
+         //farmer
+          if (occupation == 'f')
+                charisma = charisma + 1.0; 
+          if (money > 0){
+           charisma = money * .1 + charisma;   
+          }
+          System.out.println("Charisma is at " + charisma);
+        return charisma > 20.0;
+    }
+
+    
+}
