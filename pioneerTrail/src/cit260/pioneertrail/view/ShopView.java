cbc6d3fc92369685915f688e35190d6e1b9245ab
hughe <hughe@LAPTOP-D25Q3228>
2018-06-14 16:57:19 -0700

    5/21/18-added scene class

--- pioneerTrail/src/cit260/pioneertrail/view/ShopView.java
+++ pioneerTrail/src/cit260/pioneertrail/view/ShopView.java
@@ -5,14 +5,71 @@
  */
 package cit260.pioneertrail.view;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Stephen
+ * @author Hughes
  */
 public class ShopView {
     
     void displayShopView() {
-        //STUFF
+        //STUFF  System.out.print("\033[H\033[2J"); //Clears Screen
+       System.out.println(
+        "==================================================== "
+        + "\n\t\t   Shop View"
+        + "\n==================================================== "
+        + "\n\t    A - Ammo " 
+        + "\n\t    F - Food " 
+        + "\n\t    M - Medicine " 
+        + "\n\t    I - Inventory "
+        + "\n====================================================\n\n ");
     }
     
+
+ private String[] getInputs() {
+        boolean valid = false;
+        String[] inputs = new String[1];
+
+        while (valid == false){
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            input = input.trim();
+            input = input.toUpperCase();
+
+            //Put input into String
+            char y;
+            y = input.charAt(0);
+            inputs[0] = Character.toString(y);
+            
+            if(input.length() < 1){
+                System.out.println("Invalid value entered");
+                System.out.println("You must enter a non-blank value");
+                continue;
+            } 
+            if(input == "A"){
+                System.out.println("valid value entered");
+                valid = true;
+            }
+            
+            else  if(input == "B"){
+                System.out.println("valid value entered");
+                valid = true;
+            }
+            else  if(input == "M"){
+                System.out.println("valid value entered");
+                valid = true;
+            }
+            else  if(input == "I"){
+                System.out.println("valid value entered");
+                valid = true;
+            }
+            else {
+                valid = false;
+            }
+       }
+        
+        return inputs;
+    }
 }
+            
