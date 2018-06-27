fb68f4417923c0fa0f086296c2b5ed9cabb0d0ba
hughe <hughe@LAPTOP-D25Q3228>
2018-06-27 16:52:53 -0700

    5/21/18-added scene class

--- pioneerTrail/src/cit260/pioneertrail/control/GameControl.java
+++ pioneerTrail/src/cit260/pioneertrail/control/GameControl.java
@@ -5,7 +5,10 @@
  */
 package cit260.pioneertrail.control;
 
+import cit260.pioneertrail.model.Actor;
 import cit260.pioneertrail.model.Game;
+import cit260.pioneertrail.model.InventoryItem;
+import cit260.pioneertrail.model.Map;
 import cit260.pioneertrail.model.Player;
 import pioneertrail.PioneerTrail;
 
@@ -30,10 +33,37 @@ public class GameControl {
 //        return new Player();
     }
 
-    public static void createNewGame(Player player) {
+   
+    public static int createNewGame(Player player) {
+        if(player == null)
+            return -1;
+       Game game = new Game();
+        game.setPlayer(player);
+        PioneerTrail.setCurrentGame(game);
+       // Actor actor = Actor();
+       // actor.set
+        
+//actors = createActors()
+//Save the list of actors in the Game object
+//Assign an actor to the player
+//items = createItems()
+//Save the list of items in the game
+//map = createMap(noOfRows, noOfColumns, items)
+//IF map == null THEN
+// RETURN -1
+//ENDIF
+//Assign the map to the game
+//RETURN 1 // indicates success
+
         System.out.println("START NEW GAME");
     }
+    public static InventoryItem[] createItems(){
 
+        
+    }
+    public static Map createMap(int noOfRows, int noOfColumns){
+        
+    }
     public static Game saveGame(String createNewGame) {
         System.out.println("SAVE STUBB");
         return null;
