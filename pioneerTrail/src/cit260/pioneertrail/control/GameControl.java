932d75766bc37f4085b90b7aabd4cbaf56115616
Steve <stephenjcostigan@hotmail.com>
2018-06-28 22:25:38 -0500

    Merge origin/master
    Conflicts:
    	pioneerTrail/src/cit260/pioneertrail/control/GameControl.java

--- pioneerTrail/src/cit260/pioneertrail/control/GameControl.java
+++ pioneerTrail/src/cit260/pioneertrail/control/GameControl.java
@@ -9,6 +9,7 @@ import cit260.pioneertrail.model.Actor;
 import cit260.pioneertrail.model.Game;
 import cit260.pioneertrail.model.InventoryItem;
 import cit260.pioneertrail.model.ItemReference;
+import cit260.pioneertrail.model.Location;
 import cit260.pioneertrail.model.Map;
 import cit260.pioneertrail.model.Player;
 import cit260.pioneertrail.view.MapView;
@@ -113,7 +114,7 @@ public class GameControl {
         actors.add(new Actor("Samuel","Father",100.0,"Lead the Family"));
         actors.add(new Actor("Ralph","Son",80.0,"Cause a Ruckus"));
         actors.add(new Actor("Sarah","Daughter",80.0,"Be Daddy's Angel"));
-        actors.add(new Actor("Spot","The Doggy",50.0,"Get Belly Rubs"));
+        actors.add(new Actor("Spot","The Dog",50.0,"Get Belly Rubs"));
 
         return actors;
     }
@@ -122,5 +123,4 @@ public class GameControl {
         System.out.println("SAVE STUBB");
         return null;
     }
-
 }
--- pioneerTrail/src/cit260/pioneertrail/model/Location.java
+++ pioneerTrail/src/cit260/pioneertrail/model/Location.java
@@ -2,6 +2,7 @@
 package cit260.pioneertrail.model;
 
 import java.io.Serializable;
+import java.sql.Array;
 import java.util.Objects;
 
 public class Location implements Serializable{
@@ -10,8 +11,49 @@ public class Location implements Serializable{
     public int row;
     public int column;
     private int amountRemaining;
+    private boolean visited;
     
     public Location() {
+    }
+    
+    public Location [][] createLocation(int rows, int columns){
+        
+//        public static Location[][] createLocations(int rows, int columns) {
+//        IF rows < 1 OR columns < 1 THEN
+//          RETURN null
+//        ENDIF
+//        locations = new two-dimensional Location array
+//        FOR every row in the locations array
+//          FOR every column in the locations array
+//              location = create a new Location object
+//              set the row, and column attributes in the location
+//              set visited attribute to false
+//              Assign location to the row, and column in array
+//          ENDFOR
+//        RETURN locations
+//        }
+        
+        int w = 15;
+        int h = 15;
+        
+        if (rows < 1 || columns < 1) {
+            return null;
+        }
+        String[][] location = null;
+        for (int i = 0; i < h; i++) {
+            for (int k = 0; k < w; k++) {
+                location[i][k] = "String[row:"+i+",column:"+k+",column:false]";
+            }
+        }
+        return null;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
     }
 
     public Scene getScene() {
@@ -45,7 +87,7 @@ public class Location implements Serializable{
     public void setAmountRemaining(int amountRemaining) {
         this.amountRemaining = amountRemaining;
     }
-
+    
     @Override
     public int hashCode() {
         int hash = 5;
