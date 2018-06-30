675efcfe654ec246a31623d75a1e51025317709a
Steve <stephenjcostigan@hotmail.com>
2018-06-29 23:15:18 -0500

    MapControl scenes

--- pioneerTrail/src/cit260/pioneertrail/control/MapControl.java
+++ pioneerTrail/src/cit260/pioneertrail/control/MapControl.java
@@ -14,100 +14,202 @@ import cit260.pioneertrail.model.SceneType;
  * @author Stephen
  */
 public class MapControl {
-    
-//    public static Scene[] createScenes() {
-//scenes = Create an array Scene objects
-//scene1 = Create a new Scene object
-//Assign values to each attribute in the Scene object
-//Assign scene1 to its position in the scenes array
-//scene2 = Create a new ConstructionScene object
-//Assign values to each attribute in the Scene object
-//Assign scene2 to its position in the scenes array
-//scene2 = Create a new ResourceScene object
-//Assign values to each attribute in the Scene object
-//Assign scene2 to its position in the scenes array
-//…
-//…
-//…
-//RETURN scenes
-//}
-    
-    /* 
-    River,
-    Lake,
-    Waterfall,
-    Tundra,
-    Sparse,
-    CrackedEarth,
-    Arid,
-    Desert,
-    Hills,
-    Mountain,
-    Stream,
-    Town,
-    Village,
-    Encampment,
-    IndianCamp,
-    Caves,
-    Swamp,
-    DryRiver,
-    Flooded,
-    MuddyPath,
-    Zion;
-    */
-    
+
     public static Scene[] createScenes(){
-        
-//        private SceneType scene;
-//    private InventoryItem resource;
-        
         
         int s = 27;
         Scene[] scenes = new Scene[s];
         
-        int i = 0;
-        scenes[SceneType.BushLand.ordinal()] = new Scene();
-        scenes[SceneType.BushLand.ordinal()].setDescription("BushLand");
-        scenes[SceneType.BushLand.ordinal()].setBlocked(false);
-        scenes[SceneType.BushLand.ordinal()].setSymbol("B");
-        scenes[SceneType.BushLand.ordinal()].setActor(getActor(i));
-        scenes[SceneType.BushLand.ordinal()].setActor(getItem(i));
+        int o = SceneType.BushLand.ordinal();
+        scenes[o] = new Scene();
+        scenes[o].setDescription("BushLand");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("ᴥ");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
         
-        i += 1;
-        scenes[SceneType.Plains.ordinal()].setDescription("Plains");
-        scenes[SceneType.Plains.ordinal()].setBlocked(false);
-        scenes[SceneType.Plains.ordinal()].setSymbol("P");
-        scenes[SceneType.Plains.ordinal()].setActor(getActor(i));
-        scenes[SceneType.Plains.ordinal()].setActor(getItem(i));
+        o = SceneType.Plains.ordinal();
+        scenes[o].setDescription("Plains");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("؀");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
         
-        i += 1;
-        scenes[SceneType.Forest.ordinal()].setDescription("Forest");
-        scenes[SceneType.Forest.ordinal()].setBlocked(false);
-        scenes[SceneType.Forest.ordinal()].setSymbol("B");
-        scenes[SceneType.Forest.ordinal()].setActor(getActor(i));
-        scenes[SceneType.Forest.ordinal()].setActor(getItem(i));
+        o = SceneType.Forest.ordinal();
+        scenes[o].setDescription("Forest");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("ѱ");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
         
-        i += 1;
-        scenes[SceneType.Jungle.ordinal()].setDescription("Jungle");
-        scenes[SceneType.Jungle.ordinal()].setBlocked(false);
-        scenes[SceneType.Jungle.ordinal()].setSymbol("B");
-        scenes[SceneType.Jungle.ordinal()].setActor(getActor(i));
-        scenes[SceneType.Jungle.ordinal()].setActor(getItem(i));
+        o = SceneType.Jungle.ordinal();
+        scenes[o].setDescription("Jungle");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("∆");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
         
-        i += 1;
-        scenes[SceneType.Canyon.ordinal()].setDescription("Canyon");
-        scenes[SceneType.Canyon.ordinal()].setBlocked(false);
-        scenes[SceneType.Canyon.ordinal()].setSymbol("B");
-        scenes[SceneType.Canyon.ordinal()].setActor(getActor(i));
-        scenes[SceneType.Canyon.ordinal()].setActor(getItem(i));
+        o = SceneType.Canyon.ordinal();
+        scenes[o].setDescription("Canyon");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("_");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
         
-        i += 1;
-        scenes[SceneType.RedForest.ordinal()].setDescription("RedForest");
-        scenes[SceneType.RedForest.ordinal()].setBlocked(false);
-        scenes[SceneType.RedForest.ordinal()].setSymbol("B");
-        scenes[SceneType.RedForest.ordinal()].setActor(getActor(i));
-        scenes[SceneType.RedForest.ordinal()].setActor(getItem(i));
-                
+        o = SceneType.RedForest.ordinal();
+        scenes[o].setDescription("RedForest");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("▲");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.River.ordinal();
+        scenes[o].setDescription("River");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("-");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Lake.ordinal();
+        scenes[o].setDescription("Lake");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("~");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Waterfall.ordinal();
+        scenes[o].setDescription("Waterfall");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("ת");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Tundra.ordinal();
+        scenes[o].setDescription("Tundra");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("*");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+         
+        o = SceneType.Sparse.ordinal();
+        scenes[o].setDescription("Sparse");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol(",");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+         
+        o = SceneType.CrackedEarth.ordinal();
+        scenes[o].setDescription("CrackedEarth");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("ﷴ");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+         
+        o = SceneType.Arid.ordinal();
+        scenes[o].setDescription("Arid");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol(".");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Desert.ordinal();
+        scenes[o].setDescription("Desert");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("ꞈ");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Hills.ordinal();
+        scenes[o].setDescription("Hills");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("˄");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Mountain.ordinal();
+        scenes[o].setDescription("Mountain");
+        scenes[o].setBlocked(true);
+        scenes[o].setSymbol("۸");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Stream.ordinal();
+        scenes[o].setDescription("Stream");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("¬");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Town.ordinal();
+        scenes[o].setDescription("Town");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("◊");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Village.ordinal();
+        scenes[o].setDescription("Village");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("◊");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Encampment.ordinal();
+        scenes[o].setDescription("Encampment");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("◊");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.IndianCamp.ordinal();
+        scenes[o].setDescription("IndianCamp");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("◊");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Caves.ordinal();
+        scenes[o].setDescription("Caves");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("Ὧ");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Swamp.ordinal();
+        scenes[o].setDescription("Swamp");
+        scenes[o].setBlocked(true);
+        scenes[o].setSymbol("ʬ");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.DryRiver.ordinal();
+        scenes[o].setDescription("DryRiver");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("͜");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Flooded.ordinal();
+        scenes[o].setDescription("Flooded");
+        scenes[o].setBlocked(true);
+        scenes[o].setSymbol("؅");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.MuddyPath.ordinal();
+        scenes[o].setDescription("MuddyPath");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("B");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
+        o = SceneType.Zion.ordinal();
+        scenes[o].setDescription("Zion");
+        scenes[o].setBlocked(false);
+        scenes[o].setSymbol("♦");
+        scenes[o].setActor(getActor(o));
+        scenes[o].setActor(getItem(o));
+        
         return scenes;
     }
 
