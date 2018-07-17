e598eb68f7c6c4a3fe68acdc5e96f1f05c4e37c6
Marilee <marileeaustin@gmail.com>
2018-07-16 18:04:46 -0600

    Individual Assignment Marilee

--- pioneerTrail/src/cit260/pioneertrail/view/IndividualAssignments.java
+++ pioneerTrail/src/cit260/pioneertrail/view/IndividualAssignments.java
@@ -5,8 +5,10 @@
  */
 package cit260.pioneertrail.view;
 
+import cit260.pioneertrail.control.ResourceControl;
 import cit260.pioneertrail.control.HealthControl;
 import cit260.pioneertrail.exceptions.HealthControlException;
+import cit260.pioneertrail.exceptions.ResourceControlException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,8 +43,13 @@ public class IndividualAssignments extends View {
                 }
                 break;
             case "M":
-//                MainMenuView chuck = new MainMenuView();
-//                chuck.display();
+                {
+////                    try {
+////                        calcHuntingResource();
+////                    } catch (ResourceControlException ex) {
+////                        ErrorView.display(this.getClass().getName(), "Error reading input: " + ex.getMessage());
+//                    }
+                }
                 break;
             case "J":
 //                MainMenuView chuck = new MainMenuView();
@@ -60,6 +67,12 @@ public class IndividualAssignments extends View {
         this.console.println("\nDisplaying Health remaining for Actor: Spot (the dog): ...");
         int actorNumber = 4;
         HealthControl.calcHealthRemaining(actorNumber); 
+    }
+
+    private void calcHuntingResource() throws HealthControlException{
+        this.console.println("\nDisplaying Hunting results.");
+        int actorNumber = 4;
+        HealthControl.calcHealthRemaining(actorNumber);
     }
 
 }
